f03c0297ea78e32ac81f6918b504ca1b168b777d
gethsun1 <gethsun09@gmail.com>
2026-05-29 20:23:18 +0300

    Declare smooth scroll behavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-scroll-behavior="smooth"
       suppressHydrationWarning
     >
       <body className="flex min-h-full flex-col">
